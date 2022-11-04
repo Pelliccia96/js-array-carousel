7db89fdcc8d5913f12f614cc4512ec728a5b420b
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-04 16:12:19 +0100

    Fixed Basic Structure

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const images = [
-    "../img/01.webp",
-    "../img/02.webp",
-    "../img/03.webp",
-    "../img/04.webp",
-    "../img/05.webp",
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp",
 ];
 
 let currentImgIndex = 0
@@ -15,4 +15,7 @@ const btnPrev = document.getElementById("slider-btn-prev");
 const btnNext = document.getElementById("slider-btn-next");
 
 sliderImgEl.src = images[currentImgIndex];
-sliderImgEl.innerHTML = `${images[0]}`;
+
+for (let i = 0; i < images.length; i++) {
+    sliderImgEl.innerHTML = `<img src="${images[0]}">`;
+}
